9a50635e615dd23184b1bb0afb7e7bd441cb5ce3
Expenso <kmirosz6@gmail.com>
2018-10-27 15:53:33 +0200

    drawChessBoard function is rdy

--- _02/_03/drawChessBoard.js
+++ _02/_03/drawChessBoard.js
@@ -18,3 +18,35 @@
  * i tak zmodyfikuj program, aby dla dowolnej wartości tej zmiennej drukował
  * planszę o określonych wymiarach.
  */
+
+const drawChessBoard = (num = 8) => {
+    let board = '';
+
+    //minimum numer of rows is 2
+    for (let i = 1; i <= num && num >= 2; i++) {
+
+        //create boardRow
+        let boardRow = '';
+        while (boardRow.length < num) {
+            if (i % 2 == 0)
+                boardRow += ' #';
+            else
+                boardRow += '# ';
+        }
+
+        //push boradRow to Board
+        board += boardRow;
+
+        //add new lane tag
+        if (i != num)
+            board += '\n'
+    }
+    //Draw board
+    console.log(board.length > 2 ? board : 'Board is too small :(');
+}
+
+drawChessBoard(); //draw 8x8 board
+drawChessBoard(16) //draw 16x16 board
+drawChessBoard(4) //draw 4x4 board
+drawChessBoard(2) // Smallest board what you can get 2x2
+drawChessBoard(1) //Board is to small
