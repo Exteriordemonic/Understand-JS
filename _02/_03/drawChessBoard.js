46127a631810a495dd23beb343ea5071dc91b1cb
Expenso <kmirosz6@gmail.com>
2018-10-27 15:36:36 +0200

    crete task 3

---
+++ _02/_03/drawChessBoard.js
@@ -0,0 +1,20 @@
+/**
+ * Napisz program tworzący łańcuch reprezentujący siatkę elementów o wymiarach
+ * 8×8, używając znaków nowego wiersza do oddzielania poszczególnych wierszy.
+ * 60 Roz d z i a ł 2
+ * Na każdej pozycji siatki może być spacja albo znak #. Znaki te powinny tworzyć
+ * planszę do gry w szachy.
+ * Przekazanie tego łańcucha do funkcji console.log powinno dawać efekt
+ * podobny do poniższego:
+ * |# # # # |
+ * | # # # #|
+ * |# # # # |
+ * | # # # #|
+ * |# # # # |
+ * | # # # #|
+ * |# # # # |
+ * | # # # #|
+ * Po utworzeniu programu generującego taki wzór zdefiniuj zmienną size = 8
+ * i tak zmodyfikuj program, aby dla dowolnej wartości tej zmiennej drukował
+ * planszę o określonych wymiarach.
+ */
